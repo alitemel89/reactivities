c35021b7d372e4b791d31d3c21ffceb2be750419
alitemel89 <alitemel89.ce@gmail.com>
2022-11-28 18:35:56 +0300

    add CreateActivity controller

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,6 +1,5 @@
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -8,20 +7,19 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly IMediator _mediator;
-
-        public ActivitiesController(IMediator mediator) 
-        {
-            _mediator = mediator;
-        }
-
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities() => await _mediator.Send(new List.Query());
+        public async Task<ActionResult<List<Activity>>> GetActivities() => await Mediator.Send(new List.Query());
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query{Id = id});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
         }
     }
 }
